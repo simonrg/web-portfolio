de8e15b42ac05f9c4ea5815a44af8721819ce9fa
Simon Greene <simon_rg@yahoo.com.au>
2017-04-23 15:31:49 +1000

    Header updates, new font, introduction text

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 
-	
+	$('.site-hero').on('mouseenter', function(){
+		$('.site-hero__preview').addClass('site-hero__preview--expanded');
+		$('.site-hero__preview').children().css('visibility', 'visible');
+	});
+
+	$('.site-hero').on('mouseleave', function(){
+		$('.site-hero__preview').removeClass('site-hero__preview--expanded');
+		$('.site-hero__preview').children().css('visibility', 'hidden');
+	});
 
 });
