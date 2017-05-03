3cfbdfafbc479305b5d5cceb4f4a06238e9e4bef
Simon Greene <simon_rg@yahoo.com.au>
2017-05-03 13:45:17 +1000

    Contact form input validation

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
+	footerYear();
+
+	homepageBanner();
+
+	formValidation();
+});
+
+
+
+function footerYear() {
 	var d = new Date();
 	$('.date').text(d.getFullYear());
+}
 
+function homepageBanner() {
 	$('.site-hero').on('mouseenter', function(){
 		$('.site-hero__preview').addClass('site-hero__preview--expanded');
 		setTimeout(function(){ $('.site-hero__preview').children().css('visibility', 'visible'); }, 100);
@@ -11,5 +23,23 @@ $(document).ready(function(){
 		$('.site-hero__preview').removeClass('site-hero__preview--expanded');
 		setTimeout(function(){ $('.site-hero__preview').children().css('visibility', 'hidden'); }, 100);
 	});
+}
 
-});
+function formValidation() {
+	var name = $('.form-field__item')[0];
+	var email = $('.form-field__item')[1];
+	var message = $('.form-field__item')[2];
+
+	$('.form-field__item').on('input', function(){
+		if(name.value.length > 0 && email.value.length > 0 &&
+			message.value.length > 0 && $('.submit-btn').hasClass('submit-btn--disabled')) {
+			$('.submit-btn').removeAttr('disabled');
+			$('.submit-btn').toggleClass("submit-btn--disabled submit-btn--enabled");
+		}
+
+		if($(this).val().length == 0 && $('.submit-btn').hasClass('submit-btn--enabled')) {
+			$('.submit-btn').attr('disabled', true);
+			$('.submit-btn').toggleClass("submit-btn--enabled submit-btn--disabled");
+		}
+	});
+}
