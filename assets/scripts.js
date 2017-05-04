e7258b4be88c8abc797a5802a663f5c038c10842
Simon Greene <simon_rg@yahoo.com.au>
2017-05-04 11:44:15 +1000

    Banner text button

--- assets/scripts.js
+++ assets/scripts.js
@@ -14,12 +14,14 @@ function footerYear() {
 }
 
 function homepageBanner() {
-	$('.site-hero').on('mouseenter', function(){
+	$('.site-hero__button').on('click', function(){
+		$(this).addClass('site-hero__button--transition');
 		$('.site-hero__preview').addClass('site-hero__preview--expanded');
 		setTimeout(function(){ $('.site-hero__preview').children().css('visibility', 'visible'); }, 100);
 	});
 
 	$('.site-hero').on('mouseleave', function(){
+		$('.site-hero__button').removeClass('site-hero__button--transition');
 		$('.site-hero__preview').removeClass('site-hero__preview--expanded');
 		setTimeout(function(){ $('.site-hero__preview').children().css('visibility', 'hidden'); }, 100);
 	});
